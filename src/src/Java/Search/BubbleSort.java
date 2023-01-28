b7394d035653e3f89c932c5c80fea7940889b5ab
Jeevak Lonare <lonarejeevak@gmail.com>
2023-01-28 08:56:08 +0530

    -

---
+++ src/src/Java/Search/BubbleSort.java
@@ -0,0 +1,25 @@
+package Java.Search;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] a = {50, 25, 100, 3, 39};
+        int[] result = bubbleSort(a);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+
+    public static int[] bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
+
